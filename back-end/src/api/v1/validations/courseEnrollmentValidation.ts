ee15037c7c215de924ad7fde670994247f7768d0
csebineza-rrc <csebineza@academic.rrc.ca>
2025-12-29 22:30:20 -0600

    Update semester validation to require a non-empty string

--- back-end/src/api/v1/validations/courseEnrollmentValidation.ts
+++ back-end/src/api/v1/validations/courseEnrollmentValidation.ts
@@ -61,7 +61,7 @@ export const courseSchemas: Record<string, RequestSchemas> = {
                 "any.required": "Course ID is required",
                 "string.empty": "Course ID cannot be empty",
             }),
-            semester: Joi.string().min(0).required().messages({
+            semester: Joi.string().required().messages({
                 "any.required": "Instructor is required",
                 "number.empty": "Instructor cannot be empty",
                 "number.min": "Instructor must be greater than zero",
