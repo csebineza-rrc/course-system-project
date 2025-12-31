a983757cbb72a615f3ceba3f59255e8033da5c27
csebineza-rrc <csebineza@academic.rrc.ca>
2025-12-31 15:38:49 -0600

    Add mailer utility and package configuration for email functionality

---
+++ back-end/src/api/v1/utils/mailer.ts
@@ -0,0 +1,36 @@
+import nodemailer from 'nodemailer'; 
+import dotenv from 'dotenv'; 
+
+dotenv.config(); 
+
+// Create transporter and send email 
+export const sendEmail = async (to: string, subject: string, htmlContent: string) => { 
+  try { 
+
+    // Create a temporary Ethereal account 
+    const tempAccount = await nodemailer.createTestAccount(); 
+
+    // Create transporter using SMTP 
+    const transporter = nodemailer.createTransport({ 
+    host: 'smtp.ethereal.email', 
+    port: 587, 
+    auth: { 
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    } 
+}); 
+
+    // Send email 
+    await transporter.sendMail({ 
+      from: '"Academic Advisor" <academic-advisor@outlook.com>', 
+      to, 
+      subject, 
+      html: htmlContent, 
+    }); 
+
+    console.log("Email sent successfully"); 
+  } catch (error) { 
+    console.error("Error sending email:", error); 
+    throw new Error; 
+  } 
+}; 
